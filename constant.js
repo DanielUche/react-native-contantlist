0b3b66ec864cde2f4d262f8f2f524da71a45bba3
DanielUche <dank.uche@yahoo.com>
2019-02-24 14:48:49 +0100

    moved page naming to constants

---
+++ constant.js
@@ -0,0 +1,33 @@
+const pages = {};
+pages.sign_up = {
+  page: 'sign-up',
+  name: 'contactlist.SignUpScreen',
+  id: 'SignUpScreen',
+};
+pages.sign_in = {
+  page: 'sign-in',
+  name: 'contactlist.SignInScreen',
+  id: 'SignInScreen',
+};
+pages.home = {
+  page: 'home',
+  name: 'contactlist.HomeScreen',
+  id: 'HomeScreen',
+};
+pages.start = {
+  page: 'start',
+  name: 'contactlist.StartScreen',
+  id: 'StartScreen',
+};
+pages.menu = {
+  page: 'menu',
+  name: 'contactlist.MenuDrawer',
+  id: 'MenuDrawer',
+};
+
+export const APP_PAGES = pages;
+
+
+export const colour = {
+  primaryColor: '',
+};
